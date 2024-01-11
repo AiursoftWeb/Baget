3972b34c79c805f01c0e2b8bb545534bdf707983
Anduin Xue <xuef@microsoft.com>
2024-01-11 07:57:22 +0000

    Drop docker compose.

--- src/BaGet.Web/Program.cs
+++ src/BaGet.Web/Program.cs
@@ -7,7 +7,13 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        var app = App<Startup>(args);
+        var app = App<Startup>(args, configureBuilder: builder =>
+        {
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.Limits.MaxRequestBodySize = null;
+            });
+        });
         await app.RunMigrationsAsync();
         await app.RunAsync();
     }
