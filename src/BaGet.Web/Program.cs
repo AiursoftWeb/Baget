59ede23558710134ca82f690b8e0d4a93257bddf
AnduinXue <anduin@aiursoft.com>
2024-03-01 16:37:44 +0000

    Add code documentation in BaGet.Web Program.cs
    This commit includes additional inline comments for clarity in the Program.cs file within the BaGet.Web directory. The primary focus is on explaining the purpose and use of the 'Program' class as well as its 'Main' function. These comments serve as the entry point documentation for the BaGet application.

--- src/BaGet.Web/Program.cs
+++ src/BaGet.Web/Program.cs
@@ -4,8 +4,16 @@ using static Aiursoft.WebTools.Extends;
 
 namespace Aiursoft.BaGet.Web;
 
+/// <summary>
+/// The Program class represents the entry point for the BaGet application.
+/// </summary>
 public class Program
 {
+    /// <summary>
+    /// The entry point for the BaGet application.
+    /// </summary>
+    /// <param name="args">The command line arguments.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     public static async Task Main(string[] args)
     {
         var app = App<Startup>(args, configureBuilder: builder =>
