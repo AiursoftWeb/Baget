4065cba84d5801ca43735bb167da2a4fa09bd294
xuef <xuef@microsoft.com>
2024-03-15 07:48:15 +0000

    Upgrade packages.

--- src/BaGet.Web/Program.cs
+++ src/BaGet.Web/Program.cs
@@ -1,5 +1,6 @@
 using Aiursoft.BaGet.Database.Sqlite;
 using Aiursoft.DbTools;
+using Aiursoft.WebTools.OfficialPlugins;
 using static Aiursoft.WebTools.Extends;
 
 namespace Aiursoft.BaGet.Web;
@@ -16,13 +17,7 @@ public class Program
     /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     public static async Task Main(string[] args)
     {
-        var app = App<Startup>(args, configureBuilder: builder =>
-        {
-            builder.WebHost.ConfigureKestrel(options =>
-            {
-                options.Limits.MaxRequestBodySize = null;
-            });
-        });
+        var app = await AppAsync<Startup>(args, plugins: [new DockerPlugin(), new MaxBodySizePlugin()]);
         await app.UpdateDbAsync<SqliteContext>(UpdateMode.MigrateThenUse);
         await app.RunAsync();
     }
